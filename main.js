d43adedf0628d4c4ca06dd630ac32fa0eb4bcf58
Jesse Gould <jesse.gould@codeimmersives.com>
2019-10-23 08:44:09 -0400

    green lights'

--- main.js
+++ main.js
@@ -1,22 +1,119 @@
-function doubleAll() {
+function doubleAll(arr) {
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    
+    newArr.push(arr[i] * 2);
+  }
+
+  return newArr;
+
 }
 
-function absoluteValues() {
+function absoluteValues(arr) {
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0){
+      newArr.push(arr[i] * -1)
+    }
+    else {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr;
+
 }
 
-function yelledGreetings() {
+function yelledGreetings(arr) {
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    
+    newArr.push(arr[i] +'!');
+  }
+
+  return newArr;
 }
 
-function changeToInitials() {
+function changeToInitials(names) {
+    const initialsArr = [];
+    
+    for (let i = 0; i < names.length; i++){
+     
+        let theName = names[i];
+        let theLetter = '';
+  
+        for (let i = 0; i < theName.length; i++){
+          if (i === 0 || theName[i-1] === ' '){
+            
+          theLetter += theName[i];   
+      } 
+      
+    }initialsArr.push(theLetter)
+  }return initialsArr;
 }
 
-function doubleOdd() {
+
+function doubleOdd(num) {
+  const newArr = [];
+
+  for (let i = 0; i < num.length; i++) {
+    if(num[i] % 2 ===1 || num[i] % 2 === -1){
+      newArr.push(num[i] * 2);
+    }
+    else{
+    newArr.push(num[i]);
+    }
+  }
+
+  return newArr;
+
 }
 
-function upperCaseFirstLetters() {
+function upperCaseFirstLetters(name) {
+  const newArr = [];
+  for (let i = 0; i < name.length; i++) {
+  const theName = name[i] 
+  let theLetter = '' 
+
+  for (let i = 0; i < theName.length; i++ ){
+    if(i === 0){ 
+    theLetter +=  theName[0].toUpperCase();
+
+    }
+    else {
+    theLetter += theName[i].toLowerCase();
+}
+}newArr.push(theLetter);
+
+  }return newArr; 
 }
 
-function add1ToLeft() {
+
+function add1ToLeft(numArr) {
+  const newArr = [];
+
+  for (let i = 0; i < numArr.length; i++){
+    if (numArr[i]<0){
+
+      let num = numArr[i].toString();
+      let newNum = '';
+
+      for (let i = 0; i < num.length; i++){
+        if (num[i] === '-'){
+        newNum = newNum + num[i]+'1';
+        } else{
+
+        newNum = newNum + num[i]
+        }
+      } newArr.push(parseInt(newNum));
+    } else {
+
+
+    newArr.push(parseInt('1' + numArr[i]));
+    }
+  }return newArr;
 }
 
 
